5769013ea45e0efe8eb29cba38338edadbd54690
Jayson Buquia <buquia.jace@gmail.com>
2018-01-16 05:07:45 +0800

    Initial compile: header

---
+++ public/scripts/app.js
@@ -0,0 +1,186 @@
+(function () {
+'use strict';
+
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
+
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
+
+
+
+
+
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
+
+var createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
+
+var Navigation = function () {
+  function Navigation() {
+    classCallCheck(this, Navigation);
+
+    this.$nav = $('.nav');
+    this.$navToggle = $('.nav__toggle');
+    this.$navClose = $('.nav__close');
+    this.$dropdown = this.$nav.find('.nav__dropdown');
+  }
+
+  createClass(Navigation, [{
+    key: 'init',
+    value: function init() {
+      var _this = this;
+      this.$navToggle.click(function (e) {
+        e.preventDefault();
+        _this.$nav.addClass('nav--visible');
+      });
+      this.$navClose.click(function (e) {
+        e.preventDefault();
+        _this.$nav.removeClass('nav--visible');
+      });
+      this.$dropdown.each(function () {
+        var $dropdown = $(this);
+        $dropdown.children('a').click(function (e) {
+          e.preventDefault();
+          $dropdown.toggleClass('nav__dropdown--expanded');
+        });
+      });
+    }
+  }]);
+  return Navigation;
+}();
+
+var Nav = new Navigation();
+
+$(document).ready(function () {
+  Nav.init();
+});
+
+}());
+//# sourceMappingURL=app.js.map

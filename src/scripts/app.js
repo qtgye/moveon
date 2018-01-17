1f686e71207ea064436f58150d5cefad2587cde6
Jayson Buquia <buquia.jace@gmail.com>
2018-01-17 19:38:18 +0800

    Modal interaction

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,11 @@
 import Nav from './components/nav';
 import HomeCarousel from './components/homeCarousel';
 import Form from './components/Form';
+import Modal from './components/Modal';
 
 $(document).ready(function () {
   Nav.init();
   HomeCarousel.init();
   Form.init();
+  Modal.init();
 });
---
+++ src/scripts/components/modal.js
@@ -0,0 +1,59 @@
+class Modal {
+
+  constructor($element) {
+
+    this.$element = $($element);
+    this.$close = this.$element.find('.modal__close');
+    this.id = this.$element.attr('id');
+
+    if ( this.id ) {
+      this.$toggle = $('[data-modal='+this.id+']');
+      this.bindToggle();
+    }
+
+    this.bindClose();
+
+  }
+
+
+  bindClose() {
+    let _this = this;
+    this.$close.on('click', function () {
+      _this.hide()
+    });
+  }
+
+
+  bindToggle() {
+    let _this = this;
+    this.$toggle.on('click', function () {
+      _this.show();
+    });
+  }
+
+
+  show() {
+    this.$element.fadeIn();
+  }
+
+  hide() {
+    this.$element.fadeOut();
+  }
+
+}
+
+
+
+export default {
+
+  init() {
+
+    let $modals = $('.modal');
+
+    $modals.each(function (index, modal) {
+      let _form = new Modal(modal);
+    });
+
+  }
+
+}
